f98f28f7e17e8673e747b86a147c868153953f2b
surya <vijaykumarbhai7379@gmail.com>
2024-01-03 14:11:34 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This ML Project will be help to traverler in finding his/her Cap ',
+    author='Surya',
+    license='',
+)
